9e19aca69d663c65e6801bb1b94fb907bd427134
Valeriu Timbuc <valeriu@vtimbuc.dev>
2021-06-28 10:41:26 +0300

    remove ja

--- gatsby-config.js
+++ gatsby-config.js
@@ -47,7 +47,7 @@ module.exports = {
         // language JSON resource path
         path: `${__dirname}/src/intl`,
         // supported language
-        languages: [`en`, `ja`],
+        languages: [`en`],
         // language file path
         defaultLanguage: `en`,
         // option to redirect to `/ko` when connecting `/`
