00df2fb7ed34b1812d1fbb210ce8d5b6d634fea9
Valeriu Timbuc <valeriu@vtimbuc.dev>
2021-07-02 11:22:40 +0300

    add smooth scrollto

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,6 @@
 import React from 'react'
+import { gsap } from 'gsap'
+import { ScrollToPlugin } from 'gsap/ScrollToPlugin'
 import logoSvg from '../assets/images/logo.svg'
 import navBarSvg from '../assets/images/nav-bar.svg'
 import headerShapes765wPng from '../assets/images/header-shapes-765w.png'
@@ -7,8 +9,42 @@ import headerShapes2295wPng from '../assets/images/header-shapes-2295w.png'
 import headerShapes3060wPng from '../assets/images/header-shapes-3060w.png'
 import { useVideoModal } from './VideoModal'
 
+gsap.registerPlugin(ScrollToPlugin)
+
 export const Header = ({ title, links }) => {
   const setVideoModal = useVideoModal()
+
+  // Detect if a link's href goes to the current page
+  const getSamePageAnchor = (link) => {
+    if (
+      link.protocol !== window.location.protocol ||
+      link.host !== window.location.host ||
+      link.pathname !== window.location.pathname ||
+      link.search !== window.location.search
+    ) {
+      return false
+    }
+
+    return link.hash
+  }
+
+  // Scroll to a given hash, preventing the event given if there is one
+  function scrollToHash(hash, event) {
+    const el = hash ? document.querySelector(hash) : false
+
+    if (el) {
+      if (event) event.preventDefault()
+
+      gsap.to(window, {
+        scrollTo: { y: el, autoKill: true },
+        duration: 1,
+        ease: 'power4.inOut',
+      })
+    }
+  }
+
+  const handleLinkClick = (event) =>
+    scrollToHash(getSamePageAnchor(event.target), event)
 
   return (
     <header className="Header">
@@ -35,7 +71,7 @@ export const Header = ({ title, links }) => {
             <ul className="Header__navbar-links">
               {links.map(({ title, href, target }) => (
                 <li key={title}>
-                  <a href={href} target={target}>
+                  <a href={href} target={target} onClick={handleLinkClick}>
                     {title}
                   </a>
                 </li>
